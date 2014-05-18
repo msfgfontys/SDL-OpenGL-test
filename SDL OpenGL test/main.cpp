f5fbc9e5d961eebe12a2ed21f8e150e0a4f5ca11
Michael S <m.schifferling@fontys.nl>
2014-05-18 22:17:51 +0200

    Initial Commit

---
+++ SDL OpenGL test/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  SDL OpenGL test
+//
+//  Created by Michael Schifferling on 18/05/14.
+//  Copyright (c) 2014 Michael Schifferling. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
